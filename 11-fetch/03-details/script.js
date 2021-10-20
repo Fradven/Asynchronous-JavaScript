bca2eff7cc36cb1e02d8ce9302c2a7322be0957f
Frédéric Van Overmeire <frederic.vanovermeire@gmail.com>
2021-10-20 11:26:26 +0200

    11.03 done

--- 11-fetch/03-details/script.js
+++ 11-fetch/03-details/script.js
@@ -10,5 +10,19 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    const tpl = document.querySelector("#tpl-hero");
+    const target = document.querySelector("#target");
+    const heroID = document.querySelector("#hero-id")
+
+    document.querySelector('#run').addEventListener("click", async () => {
+        const response = await fetch("http://localhost:3000/heroes/"+heroID.value);
+        const responseJson = await response.json()
+
+        let tplImport = document.importNode(tpl.content, true)
+        tplImport.querySelector('.name').textContent = responseJson.name 
+        tplImport.querySelector('.alter-ego').textContent = responseJson.alterEgo
+        tplImport.querySelector('.powers').textContent = responseJson.abilities 
+        target.appendChild(tplImport) 
+        console.log(tplImport)
+    });
 })();
